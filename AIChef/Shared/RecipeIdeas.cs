1165c9d046f3e829e6f96861db7127b4c50ec9e3
zzhang025 <zhangzx45@gmail.com>
2023-10-22 11:34:22 -0700

    add state management

---
+++ AIChef/Shared/RecipeIdeas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AIChef.Shared
+{
+	public class RecipeIdeas
+	{
+		public int index { get; set; }
+		public string? title { get; set; }
+		public string? description { get; set; }
+	}
+}
+
